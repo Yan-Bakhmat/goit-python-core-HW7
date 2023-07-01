99df54e593108443090be0c5a19d62f806f23cae
Yan-Bakhmat <yan.bakhmat@gmail.com>
2023-07-01 20:45:29 +0200

    Update setup.py

--- clean_folder/setup.py
+++ clean_folder/setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name="clean_folder",
-    version=11.0,
+    version=1.0,
     description="This is a sorter for files, it sorts files by folders and rename the folders according to the names of files",
-    url="https://github.com/leegosx/hw_06/blob/main/sorter.py",
+    url="https://github.com/Yan-Bakhmat/goit-python-core-HW7.git",
     author="Yan Bakhmat",
     author_email="yan.bakhmat@gmail.com",
     packages=find_namespace_packages(),
