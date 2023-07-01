600487b9b863e7d83c428f081826799cabed2511
Yan-Bakhmat <yan.bakhmat@gmail.com>
2023-07-01 21:10:38 +0200

    Update setup.py

--- clean_folder/setup.py
+++ clean_folder/setup.py
@@ -11,7 +11,7 @@ setup(
     include_package_data=True,
     entry_points={
         'console_scripts':  [
-            'clean-folder = clean_folder.clean:main',
+            'clean-folder = clean_folder.clean:run',
         ],
 
     },
